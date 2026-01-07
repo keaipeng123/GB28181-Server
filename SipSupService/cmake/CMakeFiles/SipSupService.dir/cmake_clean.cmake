ada491786272126ef6691a4ea656d35ff630f53d
李新鹏 <824970451@qq.com>
2026-01-07 10:26:41 +0800

    day1笔记+代码库搭建@other

---
+++ SipSupService/cmake/CMakeFiles/SipSupService.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SipSupService.dir/home/GB28181-Server/SipSupService/src/main.o"
+  "CMakeFiles/SipSupService.dir/home/GB28181-Server/SipSupService/src/main.o.d"
+  "SipSupService"
+  "SipSupService.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SipSupService.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ SipSupService/src/main.cpp
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Hello SipSupService!" << endl;
+    return 0;
+}
